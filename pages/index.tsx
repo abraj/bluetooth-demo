5ca3d86213bcfaf5b4798c00cc6d3fe1b6c789cd
Abhishek Raj <abhi@raj.me>
2023-01-17 13:21:23 +0530

    t6.11

--- pages/index.tsx
+++ pages/index.tsx
@@ -49,18 +49,16 @@ const startScan = async (setLogs: Function, setElogs: Function) => {
       setLogs((v: string[]) => [...v, `B: ${device.gatt?.connected}`]);
 
       if (server) {
-        console.log(`C: ${device.gatt?.connected}`);
-        setLogs((v: string[]) => [...v, `C: ${device.gatt?.connected}`]);
+        console.log(`C: ${server.connected}`);
+        setLogs((v: string[]) => [...v, `C: ${server.connected}`]);
 
         // const service = await device.gatt?.getPrimaryService(0xaf84);
         // const service = await device.gatt?.getPrimaryService('battery_service');
         const service = await device.gatt?.getPrimaryService('heart_rate');
         // const service = await server.getPrimaryService('battery_service');
+
         console.log('service:', service);
         setLogs((v: string[]) => [...v, `${service}`]);
-
-        console.log(`D: ${device.gatt?.connected}`);
-        setLogs((v: string[]) => [...v, `D: ${device.gatt?.connected}`]);
 
         // const service = await server.getPrimaryService('battery_service');
         // console.log('service:', service);
@@ -112,7 +110,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <h2>Bluetooth Demo - v6.10</h2>
+        <h2>Bluetooth Demo - v6.11</h2>
         <div className={styles.section}>
           <button onClick={() => startScan(setLogs, setElogs)}>Start Scan</button>
         </div>
