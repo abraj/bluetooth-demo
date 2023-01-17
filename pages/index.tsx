a824b845d3b6010649373d65bfef7998868ea608
Abhishek Raj <abhi@raj.me>
2023-01-17 12:25:28 +0530

    t6

--- pages/index.tsx
+++ pages/index.tsx
@@ -41,12 +41,29 @@ const startScan = async (setLogs: Function, setElogs: Function) => {
       // Attempts to connect to remote GATT Server.
       console.log(`A: ${device.gatt?.connected}`);
       setLogs((v: string[]) => [...v, `A: ${device.gatt?.connected}`]);
-      await device.gatt?.connect();
+      const server = await device.gatt?.connect();
       console.log(`B: ${device.gatt?.connected}`);
       setLogs((v: string[]) => [...v, `B: ${device.gatt?.connected}`]);
 
-      // const xx = await device.gatt?.getPrimaryServices('4hhrN3cyNCPf/LO+VNu1ww==');
-      // console.log('>>', xx);
+      if (server) {
+        const service = await server.getPrimaryService('battery_service');
+        console.log('service:', service);
+        setLogs((v: string[]) => [...v, `${service}`]);
+
+        const characteristic = await service.getCharacteristic('battery_level');
+        console.log('characteristic:', characteristic);
+        setLogs((v: string[]) => [...v, `${characteristic}`]);
+
+        const batteryLevel = await characteristic.readValue();
+        console.log('batteryLevel:', batteryLevel);
+        setLogs((v: string[]) => [...v, `${batteryLevel}`]);
+
+        console.log('DONE!');
+        setLogs((v: string[]) => [...v, 'DONE!']);
+
+        // const xx = await device.gatt?.getPrimaryServices('4hhrN3cyNCPf/LO+VNu1ww==');
+        // console.log('>>', xx);
+      }
 
       // ...
     } catch (err: any) {
@@ -74,7 +91,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <h2>Bluetooth Demo - v5.5</h2>
+        <h2>Bluetooth Demo - v6</h2>
         <div className={styles.section}>
           <button onClick={() => startScan(setLogs, setElogs)}>Start Scan</button>
         </div>
