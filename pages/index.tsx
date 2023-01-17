bb1b49f859b6d7f4aad86e57bc336235dd73fc84
Abhishek Raj <abhi@raj.me>
2023-01-17 16:38:01 +0530

    t7.5

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,21 +15,21 @@ const startScan = async (setLogs: Function, setElogs: Function) => {
       });
 
       const device = await navigator.bluetooth.requestDevice({
-        // acceptAllDevices: true,
-        filters: [{
+        acceptAllDevices: true,
+        // filters: [{
           // name: 'MacBook Pro',
           // namePrefix: 'MacBook',
           // services: ['battery_service'],
           // services: ['heart_rate'],
           // services: [0xaf84],
-          services: ['00001108-0000-1000-8000-00805f9b34fb'],
+          // services: ['00001108-0000-1000-8000-00805f9b34fb'],
           // services: [0x1234, 0x12345678, '99999999-0000-1000-8000-00805f9b34fb'],
           // manufacturerData: [{
           //   companyIdentifier: 0x00e0,
           //   dataPrefix: new Uint8Array([0x01, 0x02])
           // }],
-        }],
-        // optionalServices: [0xaf84, 'battery_service', 'heart_rate'],
+        // }],
+        optionalServices: [0xaf84, 'battery_service', 'heart_rate', '00001108-0000-1000-8000-00805f9b34fb'],
         // optionalServices: [0xaf84],
         // optionalServices: ['battery_service'], // Required to access service later.
         // optionalServices: ['heart_rate'], // Required to access service later.
@@ -76,6 +76,14 @@ const startScan = async (setLogs: Function, setElogs: Function) => {
         }).catch(err => {
           console.error('[heart_rate:error]', err);
           setElogs((v: string[]) => [...v, 'heart_rate:error:' + err.toString()]);    
+        });
+
+        server.getPrimaryService('00001108-0000-1000-8000-00805f9b34fb').then(service => {
+          console.log('5f9b34fb:', service);
+          setLogs((v: string[]) => [...v, `5f9b34fb: ${service}`]);
+        }).catch(err => {
+          console.error('[5f9b34fb:error]', err);
+          setElogs((v: string[]) => [...v, '5f9b34fb:error:' + err.toString()]);    
         });
 
         // const service = await device.gatt?.getPrimaryService(0xaf84);
@@ -136,7 +144,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <h2>Bluetooth Demo - v7.3</h2>
+        <h2>Bluetooth Demo - v7.5</h2>
         <div className={styles.section}>
           <button onClick={() => startScan(setLogs, setElogs)}>Start Scan</button>
         </div>
